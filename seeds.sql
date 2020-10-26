0894b6b29cf8a3dbf6cc7dbfb63b189c41146143
R0nG13 <67929861+R0nG13@users.noreply.github.com>
2020-10-25 23:40:42 -0400

    Final Push

--- seeds.sql
+++ seeds.sql
@@ -5,15 +5,15 @@ INSERT INTO role (title, salary, department_id)
 VALUES
     ('Advising Coordinator', 85000, 1),
     ('Advisor', 45000, 1),
-    ('Admissions Coordinator', 519692, 2),
-    ('Admissions Specialist', 78820, 2),
-    ('Financial Aid Coordinator', 65813, 3),
-    ('Financial Aid Specialist', 117927, 3),
-    ('Director of Human Resources', 97777, 4),
-    ('Human Resources Specialist', 49149, 4),
-    ('Software Engineer', 105090, 5),
-    ('IT Technician', 74664, 5),
-    ('IT Director', 123900, 5);
+    ('Admissions Coordinator', 80000, 2),
+    ('Admissions Specialist', 40000, 2),
+    ('Financial Aid Coordinator', 75000, 3),
+    ('Financial Aid Specialist', 35000, 3),
+    ('Director of Human Resources', 70000, 4),
+    ('Human Resources Specialist', 30000, 4),
+    ('Software Engineer', 90000, 5),
+    ('IT Technician', 55000, 5),
+    ('IT Director', 150000, 5);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
@@ -31,4 +31,3 @@ VALUES
     ('Piotr', 'Rasputin', 9, 11),
     ('Johnny', 'Storm', 9, 11),
     ('Eric', 'Twinge', 10, 11);
-    
